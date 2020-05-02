17feb4f0530eea045b16187cb0a2a7de9644bd92
Artem Korsakov <kor@infra.guru>
2020-05-02 18:38:00 +0300

    Quadratic Equation

--- src/main/java/com/github/artemkorsakov/equations/QuadraticEquation.java
+++ src/main/java/com/github/artemkorsakov/equations/QuadraticEquation.java
@@ -1,6 +1,9 @@
 package com.github.artemkorsakov.equations;
 
 import com.github.artemkorsakov.numbers.ComplexNumber;
+import com.github.artemkorsakov.operations.Squares;
+
+import java.util.Arrays;
 
 /**
  * a*x^2 + b*x + c = 0;
@@ -11,21 +14,70 @@ public class QuadraticEquation {
     /**
      * x = (-b +/- sqrt(b^2 - 4ac))/2a
      */
-    public static int[] getSolutionsInIntegers(int a, int b, int c) {
-        return null;
+    public static long[] getSolutionsInIntegers(long a, long b, long c) {
+        long d = b * b - 4 * a * c;
+        long den = 2 * a;
+        if (d < 0 || !Squares.isSquare(d)) {
+            return new long[]{};
+        }
+
+        long sqrt = Math.round(Math.sqrt(d));
+        if (sqrt == 0) {
+            return b % den == 0 ? new long[]{-b / den} : new long[]{};
+        }
+
+        if ((-b + sqrt) % den == 0 && (-b - sqrt) % den == 0) {
+            long[] solutions = new long[2];
+            solutions[0] = (-b + sqrt) / den;
+            solutions[1] = (-b - sqrt) / den;
+            return solutions;
+        }
+        if ((-b + sqrt) % den == 0) {
+            return new long[]{(-b + sqrt) / den};
+        }
+        if ((-b - sqrt) % den == 0) {
+            return new long[]{(-b - sqrt) / den};
+
+        }
+        return new long[]{};
     }
 
     /**
      * x = (-b +/- sqrt(b^2 - 4ac))/2a
      */
-    public static double[] getSolutionsInDoubles(int a, int b, int c) {
-        return null;
+    public static double[] getSolutionsInDoubles(double a, double b, double c) {
+        double d = b * b - 4 * a * c;
+        double den = 2 * a;
+        if (d < 0) {
+            return new double[]{};
+        }
+
+        double sqrt = Math.sqrt(d);
+        if (sqrt == 0) {
+            return new double[]{-b / den};
+        }
+
+        double[] solutions = new double[2];
+        solutions[0] = (-b + sqrt) / den;
+        solutions[1] = (-b - sqrt) / den;
+        return solutions;
     }
 
     /**
      * x = -b/2a +/- (sqrt(4ac - b^2)/2a)i
      */
-    public static ComplexNumber[] getSolutionsInComplexNumbers(int a, int b, int c) {
-        return null;
+    public static ComplexNumber[] getSolutionsInComplexNumbers(double a, double b, double c) {
+        double d = b * b - 4 * a * c;
+        if (d >= 0) {
+            return Arrays.stream(getSolutionsInDoubles(a, b, c))
+                    .mapToObj(s -> new ComplexNumber(s, 0.0)).toArray(ComplexNumber[]::new);
+        }
+
+        double sqrt = Math.sqrt(-d);
+        double den = 2 * a;
+        ComplexNumber[] solutions = new ComplexNumber[2];
+        solutions[0] = new ComplexNumber(-b / den, sqrt / den);
+        solutions[1] = new ComplexNumber(-b / den, -sqrt / den);
+        return solutions;
     }
 }
--- src/test/java/com/github/artemkorsakov/equations/QuadraticEquationTests.java
+++ src/test/java/com/github/artemkorsakov/equations/QuadraticEquationTests.java
@@ -1,4 +1,86 @@
 package com.github.artemkorsakov.equations;
 
+import com.github.artemkorsakov.numbers.ComplexNumber;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class QuadraticEquationTests {
+    @Test
+    public void testGetSolutionsInIntegers() {
+        long[] solutions = QuadraticEquation.getSolutionsInIntegers(1, -5, 6);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], 3);
+        Assert.assertEquals(solutions[1], 2);
+
+        solutions = QuadraticEquation.getSolutionsInIntegers(1, -5, -6);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], 6);
+        Assert.assertEquals(solutions[1], -1);
+
+        solutions = QuadraticEquation.getSolutionsInIntegers(4, -8, 4);
+        Assert.assertEquals(solutions.length, 1);
+        Assert.assertEquals(solutions[0], 1);
+
+        solutions = QuadraticEquation.getSolutionsInIntegers(1, -5, 7);
+        Assert.assertEquals(solutions.length, 0);
+
+        solutions = QuadraticEquation.getSolutionsInIntegers(1, -5, 5);
+        Assert.assertEquals(solutions.length, 0);
+
+        solutions = QuadraticEquation.getSolutionsInIntegers(3, -4, 1);
+        Assert.assertEquals(solutions.length, 1);
+        Assert.assertEquals(solutions[0], 1);
+
+        solutions = QuadraticEquation.getSolutionsInIntegers(3, 4, 1);
+        Assert.assertEquals(solutions.length, 1);
+        Assert.assertEquals(solutions[0], -1);
+    }
+
+    @Test
+    public void testGetSolutionsInDoubles() {
+        double[] solutions = QuadraticEquation.getSolutionsInDoubles(1, -5, 6);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], 3);
+        Assert.assertEquals(solutions[1], 2);
+
+        solutions = QuadraticEquation.getSolutionsInDoubles(1, -5, -6);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], 6);
+        Assert.assertEquals(solutions[1], -1);
+
+        solutions = QuadraticEquation.getSolutionsInDoubles(1, -2, 1);
+        Assert.assertEquals(solutions.length, 1);
+        Assert.assertEquals(solutions[0], 1);
+
+        solutions = QuadraticEquation.getSolutionsInDoubles(1, -5, 7);
+        Assert.assertEquals(solutions.length, 0);
+
+        solutions = QuadraticEquation.getSolutionsInDoubles(1, -5, 5);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], (5 + Math.sqrt(5)) / 2.0);
+        Assert.assertEquals(solutions[1], (5 - Math.sqrt(5)) / 2.0);
+
+        solutions = QuadraticEquation.getSolutionsInDoubles(3, -4, 1);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], 1);
+        Assert.assertEquals(solutions[1], 1.0 / 3.0);
+
+        solutions = QuadraticEquation.getSolutionsInDoubles(3, 4, 1);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], -1.0 / 3.0);
+        Assert.assertEquals(solutions[1], -1);
+    }
+
+    @Test
+    public void testGetSolutionsInComplexNumbers() {
+        ComplexNumber[] solutions = QuadraticEquation.getSolutionsInComplexNumbers(1, -2, 2);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], new ComplexNumber(1, 1));
+        Assert.assertEquals(solutions[1], new ComplexNumber(1, -1));
+
+        solutions = QuadraticEquation.getSolutionsInComplexNumbers(3, 4, 1);
+        Assert.assertEquals(solutions.length, 2);
+        Assert.assertEquals(solutions[0], new ComplexNumber(-1.0 / 3.0, 0.0));
+        Assert.assertEquals(solutions[1], new ComplexNumber(-1, 0.0));
+    }
 }
